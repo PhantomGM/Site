a9b78dbd01266d52fc020916351fa43712c08042
Nick Dallman <101128630+PhantomGM@users.noreply.github.com>
2025-08-20 21:54:17 -0400

    test: add search filtering tests

--- src/app/(routes)/search/page.tsx
+++ src/app/(routes)/search/page.tsx
@@ -3,7 +3,7 @@ import { t } from '../../../lib/i18n'
 export default function SearchPage() {
   return (
     <div>
-      <h1>{t('map', 'off') ?? 'Search'}</h1>
+      <h1>{t('search', 'off') ?? 'Search'}</h1>
       <p>Search placeholder.</p>
     </div>
   )
